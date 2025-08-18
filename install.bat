b2dc7bf7558f3b1afcdfe50c6bfb026e7192851f
NtskwK <natsukawa247@outlook.com>
2025-08-18 18:29:31 +0000

    feat: go to target directory by bat command

--- install.bat
+++ install.bat
@@ -1,6 +1,10 @@
-d:
-cd D:\file\project\NM\narutomobile
+@echo off
+:: 将工作目录设置为当前脚本所在的目录
+cd /d "%~dp0"
+
+:: 验证是否切换成功（可选）
+echo 当前工作目录已设置为：
+echo %cd%
+
 python ./install.py
 install/MaaPiCli.exe
-
-
